318f8827f737c8f2207e7e52546bac5624cdb5ab
刘浩齐12345 <>
2022-11-15 17:07:00 +0800

    代码备份

--- app/src/main/java/com/study/composestudy/MainActivity.kt
+++ app/src/main/java/com/study/composestudy/MainActivity.kt
@@ -3,13 +3,18 @@ package com.study.composestudy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.study.composestudy.ui.theme.ComposeStudyTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +24,9 @@ class MainActivity : ComponentActivity() {
             ComposeStudyTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.fillMaxSize()
+                        .background(Color.LightGray)
+                        .border(2.dp, Color.Black),
                     color = MaterialTheme.colors.background
                 ) {
                     Greeting("Android")
@@ -31,7 +38,20 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Column(
+        modifier = Modifier.background(Color.White)
+            .fillMaxSize()
+            .width(200.dp)
+            .height(200.dp)
+    ) {
+        Text(text = "Hello $name!")
+        Box(
+            modifier = Modifier.fillMaxWidth()
+                .background(color = Color.Yellow)
+                .border(2.dp, Color.Red, shape = RoundedCornerShape(5.dp))
+                .padding(10.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
