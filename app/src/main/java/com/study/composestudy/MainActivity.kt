6069c188db047906f8c5c249b868020ca3ec0b86
刘浩齐12345 <>
2022-11-16 17:36:40 +0800

    代码备份

--- app/src/main/java/com/study/composestudy/MainActivity.kt
+++ app/src/main/java/com/study/composestudy/MainActivity.kt
@@ -11,10 +11,15 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.study.composestudy.ui.theme.ComposeStudyTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,7 +29,8 @@ class MainActivity : ComponentActivity() {
             ComposeStudyTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize()
+                    modifier = Modifier
+                        .fillMaxSize()
                         .background(Color.LightGray)
                         .border(2.dp, Color.Black),
                     color = MaterialTheme.colors.background
@@ -39,18 +45,44 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     Column(
-        modifier = Modifier.background(Color.White)
+        modifier = Modifier
             .fillMaxSize()
+            .background(Color.White)
             .width(200.dp)
             .height(200.dp)
     ) {
-        Text(text = "Hello $name!")
+        Text(
+            text = "Hello $name!",
+            style = TextStyle(
+                fontSize = 16.sp,
+                color = Color.Blue,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(40.dp)
+        )
         Box(
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .width(200.dp)
+                .height(50.dp)
                 .background(color = Color.Yellow)
                 .border(2.dp, Color.Red, shape = RoundedCornerShape(5.dp))
                 .padding(10.dp)
-        )
+                .align(Alignment.CenterHorizontally)
+        ) {
+            Text(
+                "你好，测试一下",
+                style = TextStyle(
+                    color = Color.Red,
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold
+                ),
+                modifier = Modifier
+                    .fillMaxSize()
+            )
+        }
     }
 }
 
